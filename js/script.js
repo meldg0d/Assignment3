00f4229fc233878dd2119124f5e44ea3d535f1b6
meldg0d <mankangodt@gmail.com>
2023-11-09 09:04:37 +0100

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 //MAKE THE MAGIC HAPPEN
+
+  function moveImageRandomly() {
+    var maxX = $(window).width() - $('#butterfly').width();
+    var maxY = $(window).height() - $('#butterfly').height();
+
+    var randomX = Math.floor(Math.random() * maxX);
+    var randomY = Math.floor(Math.random() * maxY);
+
+    $('#butterfly').animate({
+      left: randomX,
+      top: randomY
+    }, 3000, moveImageRandomly);
+  }
+
+  $(document).ready(function() {
+    moveImageRandomly();
+  });
